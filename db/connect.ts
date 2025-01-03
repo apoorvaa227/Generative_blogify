62a3b1fbffa07bf86e64f40cff42154316ae69e9
Apoorva <apoorva2212076@akgec.ac.in>
2025-01-03 15:41:08 +0530

    fake data generation

---
+++ db/connect.ts
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const serverSelectionTimeoutMS: number = Number(process.env.SERVER_SELECTION_TIMEOUT_MS) || 5000;
+
+const connectDB = (connectionString: string): Promise<typeof mongoose> => {
+    return mongoose.connect(connectionString, { serverSelectionTimeoutMS });
+};
+
+export default connectDB;
